21fe181d7c0bf8dc7b07e6e59b6bfff17c9421f2
vivekanandas <pingmevivek@gmail.com>
2016-09-19 18:51:06 -0400

    Revert "vivek change  git pr 3 test"
    This reverts commit 589c89007e9a485835446a212475e67bb52bfeba.

--- View.php
+++ View.php
@@ -7,7 +7,7 @@ namespace Crada\Apidoc;
 use Crada\Apidoc\View\Baseview;
 
 /**
- * @license http://opensource.org/licenses/bsd-license.php The vivek pull request 3
+ * @license http://opensource.org/licenses/bsd-license.php The vivek pull request 2
  * @author  Calin Rada <rada.calin@gmail.com>
  */
 class View extends BaseView
